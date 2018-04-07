e0c3c03256a19c3e169150c61524a2eab5d77cb2
Kagami Hiiragi <kagami@genshiken.org>
2018-04-08 01:37:50 +0300

    Disable debug

--- dlib.go
+++ dlib.go
@@ -1,6 +1,6 @@
 package dlib
 
 // #cgo pkg-config: dlib-1
-// #cgo CXXFLAGS: -std=c++1z -Wall -O3 -march=native
+// #cgo CXXFLAGS: -std=c++1z -Wall -O3 -DNDEBUG -march=native
 // #cgo LDFLAGS: -ljpeg
 import "C"
