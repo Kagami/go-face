acafdf17217de4792338f0b93ada7ce8241a5e1c
Kagami Hiiragi <kagami@genshiken.org>
2018-04-15 03:18:27 +0300

    Actually respect distance ordering

--- classify.cc
+++ classify.cc
@@ -12,37 +12,45 @@ int classify(
 	int idx = 0;
 	for (const auto& sample : samples) {
 		double dist = dist_func(sample, test_sample);
-		distances.push_back(std::move(std::make_pair(idx, dist)));
+		distances.push_back({idx, dist});
 		idx++;
 	}
 
 	std::sort(
 		distances.begin(), distances.end(),
-		[](const auto a, const auto b) -> bool { return a.second < b.second; }
+		[](const auto a, const auto b) { return a.second < b.second; }
 	);
 
 	int len = std::min((int)distances.size(), 10);
-	std::map<int, int> hits_by_cat;
+	std::unordered_map<int, std::pair<int, double>> hits_by_cat;
 	for (int i = 0; i < len; i++) {
 		int idx = distances[i].first;
+		double dist = distances[i].second;
 		auto cat = cats.find(idx);
 		if (cat == cats.end())
 			continue;
-		auto hits = hits_by_cat.find(cat->second);
-		if (hits == hits_by_cat.end()) {
-			// printf("1 hit for %d (%d: %f)\n", cat->second, idx, distances[i].second);
-			hits_by_cat[cat->second] = 1;
+		int catIdx = cat->second;
+		auto hit = hits_by_cat.find(catIdx);
+		if (hit == hits_by_cat.end()) {
+			// printf("1 hit for %d (%d: %f)\n", catIdx, idx, dist);
+			hits_by_cat[catIdx] = {1, dist};
 		} else {
-			// printf("%d hits for %d (%d: %f)\n", hits->second + 1, cat->second, idx, distances[i].second);
-			hits_by_cat[cat->second] = hits->second + 1;
+			// printf("+1 hit for %d (%d: %f)\n", catIdx, idx, dist);
+			hits_by_cat[catIdx].first++;
 		}
 	}
 
-	auto hits = std::max_element(
+	auto hit = std::max_element(
 		hits_by_cat.begin(), hits_by_cat.end(),
-		[](const auto a, const auto b) { return a.second < b.second; }
+		[](const auto a, const auto b) {
+			int hits1 = a.second.first;
+			int hits2 = b.second.first;
+			double dist1 = a.second.second;
+			double dist2 = b.second.second;
+			if (hits1 == hits2) return dist1 > dist2;
+			return hits1 < hits2;
+		}
 	);
-	// printf("Found cat with max hits: %d\n", hits->first);
-	// fflush(stdout);
-	return hits->first;
+	// printf("Found cat with max hits: %d\n", hit->first); fflush(stdout);
+	return hit->first;
 }
