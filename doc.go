ba283afe15f8f9b7aa6bf165d932c93581758a0e
Kagami Hiiragi <kagami@genshiken.org>
2018-07-30 19:37:29 +0300

    Typo fix
    [ci skip]

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 /*
-Package go-face implements face recognition for Go using dlib, a popular
+Package face implements face recognition for Go using dlib, a popular
 machine learning toolkit.
 */
 package face
