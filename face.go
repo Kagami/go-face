7c338c5174282cd92d9d46e81dc7a2a456be0cdd
Nathan Youngman <4566+nathany@users.noreply.github.com>
2019-02-26 10:29:21 -0700

    Don’t use mangled C names
    https://golang.org/doc/go1.12#cgo
    
    Fixes #22

--- face.go
+++ face.go
@@ -22,7 +22,7 @@ const (
 // A Recognizer creates face descriptors for provided images and
 // classifies them into categories.
 type Recognizer struct {
-	ptr *_Ctype_struct_facerec
+	ptr *C.facerec
 }
 
 // Face holds coordinates and descriptor of the human face.
